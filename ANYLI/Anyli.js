0e3533d7cd71047f16f136741de6f19695ccba0a
Diana Fernandez <dfernandez2829@gmail.com>
2026-07-06 17:00:06 -0400

    Pagina web Anyli

---
+++ ANYLI/Anyli.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+
+    popoverTriggerList.forEach(popoverTriggerEl => {
+        new bootstrap.Popover(popoverTriggerEl);
+    });
+});
+
